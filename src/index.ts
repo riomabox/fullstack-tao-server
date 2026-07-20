8c3705afe33f58dfebc8e89aa1426e0318836e5d
riomabox <riomabox@gmail.com>
2026-07-20 14:44:22 +0700

    Tambah middleware authenticate

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { env } from "./config/env";
 import db from "./db/index";
 import models from "./models";
@@ -11,12 +11,24 @@ const port = env.PORT;
 
 app.use(express.json());
 
+const getCurrentUser = (req) => {
+        return { id: "8256bce6-a8e0-4d7a-a1ac-305d4a0c0db4" };
+};
+
+const authenticate = (req: Request, res: Response, next: NextFunction) => {
+        const user = getCurrentUser(req);
+        if (!user) {
+                return res.status(401).json({ message: "Unauthorized" });
+        }
+        req.user = user;
+        next();
+};
+
 app.get("/", (req: Request, res: Response) => {
         res.send("Hello from Express + TypeScript!");
 });
 
-app.get("/prompts", async (req: Request, res: Response) => {
-        const user = { id: "8256bce6-a8e0-4d7a-a1ac-305d4a0c0db4" };
+app.get("/prompts", authenticate, async (req: Request, res: Response) => {
         const currentDate = new Date();
 
         let prompt;
@@ -39,7 +51,7 @@ app.get("/prompts", async (req: Request, res: Response) => {
         const hasAnswered = await db
                 .select()
                 .from(answers)
-                .where(and(eq(answers.user_id, user.id), eq(answers.prompt_id, prompt.id)));
+                .where(and(eq(answers.user_id, req.user.id), eq(answers.prompt_id, prompt.id)));
 
         if (hasAnswered.length === 0) {
                 return res.json({ ...prompt, answers: [] });
@@ -48,7 +60,7 @@ app.get("/prompts", async (req: Request, res: Response) => {
         const resultAnswers = await db
                 .select()
                 .from(answers)
-                .where(and(eq(answers.user_id, user.id), eq(answers.prompt_id, prompt.id)));
+                .where(and(eq(answers.user_id, req.user.id), eq(answers.prompt_id, prompt.id)));
         return res.status(200).json({ ...prompt, answers: resultAnswers });
 });
 
